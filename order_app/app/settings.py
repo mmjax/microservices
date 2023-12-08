1dd086ec230f62c9386536d0dd861c5bbb2368c4
mmjax <maxskgtr@gmail.com>
2023-12-08 17:37:01 +0300

    перемены)

--- order_app/app/settings.py
+++ order_app/app/settings.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     amqp_url: str
     postgres_url: str
-    port: str
+    port: str 
     model_config = SettingsConfigDict(env_file='.env')
 
 settings = Settings()
