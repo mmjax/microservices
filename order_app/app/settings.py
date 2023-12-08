abe3e573e359206e26ddc89174b8289c000bbee7
mmjax <maxskgtr@gmail.com>
2023-12-08 17:22:01 +0300

    update settings

--- order_app/app/settings.py
+++ order_app/app/settings.py
@@ -1,11 +1,9 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from dotenv import load_dotenv
-load_dotenv()
-import os
 
 class Settings(BaseSettings):
-    amqp_url: str = os.load_dotenv(AMQP_URL)
-    postgres_url: str = os.load_dotenv(POSTGRES_URL)
-    port: str = os.load_dotenv(PORT)
+    amqp_url: str
+    postgres_url: str
+    port: str
+    model_config = SettingsConfigDict(env_file='.env')
 
 settings = Settings()
