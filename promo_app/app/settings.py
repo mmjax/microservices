e63c9894fd1f1acb82637252c288d7e98b5c5c29
mmjax <maxskgtr@gmail.com>
2023-12-08 17:12:04 +0300

    update cicd

--- promo_app/app/settings.py
+++ promo_app/app/settings.py
@@ -4,5 +4,4 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     amqp_url: str = "amqp://guest:guest@microservices-rabbitmq-1:5672/"
 
-
 settings = Settings()
